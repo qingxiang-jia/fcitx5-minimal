2181c11e181b85706834958c9b84d6610cfd73a2
Qingxiang Jia <ybjqx3340@gmail.com>
2022-04-09 21:54:08 -0400

    Add methods to be done

--- minimal.hpp
+++ minimal.hpp
@@ -4,6 +4,7 @@
 #include <fcitx-utils/inputbuffer.h>
 #include <fcitx/addonfactory.h>
 #include <fcitx/addonmanager.h>
+#include <fcitx/candidatelist.h>
 #include <fcitx/inputcontext.h>
 #include <fcitx/inputcontextproperty.h>
 #include <fcitx/inputmethodengine.h>
@@ -20,6 +21,97 @@ public:
   }
 
   void select(fcitx::InputContext* inputCtx) const override
+  {
+
+  }
+};
+
+class MinCandidateList : public fcitx::CandidateList,
+                         public fcitx::PageableCandidateList
+{
+public:
+  MinCandidateList()
+  {
+
+  }
+
+  const fcitx::Text& label(int idx) const
+  {
+
+  }
+
+  const fcitx::CandidateWord& candidate(int idx) const
+  {
+
+  }
+
+  int size()
+  {
+
+  }
+
+  int cursorIndex() const
+  {
+
+  }
+
+  fcitx::CandidateLayoutHint layoutHint() const
+  {
+
+  }
+
+  bool empty() const
+  {
+
+  }
+
+  // Into PageableCandiateList
+
+  bool hasPrev() const
+  {
+
+  }
+
+  bool hasNext() const
+  {
+
+  }
+
+  void prev()
+  {
+
+  }
+
+  void next()
+  {
+
+  }
+
+  bool usedNextBefore() const
+  {
+    
+  }
+};
+
+class MinEngine : public fcitx::InputMethodEngineV2
+{
+public:
+  MinEngine(fcitx::Instance* instance)
+  {
+
+  }
+
+  void activate(const fcitx::InputMethodEntry& entry, fcitx::InputContextEvent& ctxEvent)
+  {
+
+  }
+
+  void keyEvent(const fcitx::InputMethodEntry& entry, fcitx::KeyEvent* keyEvent)
+  {
+
+  }
+
+  void reset(const fcitx::InputMethodEntry& entry, fcitx::InputContextEvent& ctxEvent)
   {
 
   }
